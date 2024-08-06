1d22027dcbd65dfcc2d1272589c78cf6b4a01a12
Babar-Jawad-Anjum <89509895+Babar-Jawad-Anjum@users.noreply.github.com>
2024-08-06 17:18:28 +0500

    feat: document middlewares added

--- Node-Express/Models/movieModel.js
+++ Node-Express/Models/movieModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
 
 const movieSchema = new mongoose.Schema(
   {
@@ -55,6 +56,7 @@ const movieSchema = new mongoose.Schema(
       type: Number,
       required: [true, "Price is required field!"],
     },
+    createdBy: String,
   },
   //This means, Each time the data is output as json, we want virtuals to be the part of output
   {
@@ -66,7 +68,29 @@ const movieSchema = new mongoose.Schema(
 //Virtual property that will not save in db, but will be calculated using existing field i.e duration
 //This will add "durationInHours" field in each document by calculating duration in hours from "hours" field
 movieSchema.virtual("durationInHours").get(function () {
+  //this pointing to the document which is currently processed
   return this.duration / 60;
+});
+
+//---------------------------------------------------
+//MongoDb middlewares or also Known as Pre or post hook.
+//EXECUTED BEFORE THE DOCUMENT IS SAVED IN DB
+// .save() or .create()
+//---------------------------------------------------
+//pre-hook
+movieSchema.pre("save", function (next) {
+  //we can extract name to token etc here, but assigned static name
+  this.createdBy = "@BABAR MUGHAL";
+  next();
+});
+
+//post-hook || This hook will not have access to this, it have access to currently saved document
+movieSchema.post("save", function (doc, next) {
+  const content = `A new movie document with name ${doc.name} has been created by ${doc.createdBy}\n`;
+  fs.writeFileSync("./Log/log.txt", content, { flag: "a" }, (err) => {
+    console.log(err.message);
+  });
+  next();
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
