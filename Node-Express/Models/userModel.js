1a2edeea58ec98e891e2f31758f03755710517f5
Babar-Jawad-Anjum <89509895+Babar-Jawad-Anjum@users.noreply.github.com>
2024-08-09 00:32:01 +0500

    feat: password hashing implemented

--- Node-Express/Models/userModel.js
+++ Node-Express/Models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,25 @@ const userSchema = new mongoose.Schema({
   confirmPassword: {
     type: String,
     required: [true, "Please confirm your password!"],
+    validate: {
+      //This validator will only work for save() & create()
+      validator: function (val) {
+        return this.password === val;
+      },
+      message: "Password & ConfirmPassword does not match!",
+    },
   },
+});
+
+//Password encryption logic
+userSchema.pre("save", async function (next) {
+  //isModified() is a built in function
+  if (!this.isModified("password")) return next();
+
+  //encrypt the password
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
